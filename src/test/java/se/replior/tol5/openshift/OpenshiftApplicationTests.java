a363936ef61b9a92c4283c05207f21e394dac58e
Mathias Fryksten <mathias@bfcglobal.com>
2017-10-03 14:33:45 +0800

    - fixed unit tests

--- src/test/java/se/replior/tol5/openshift/OpenshiftApplicationTests.java
+++ src/test/java/se/replior/tol5/openshift/OpenshiftApplicationTests.java
@@ -3,10 +3,12 @@ package se.replior.tol5.openshift;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes = {TestConfiguration.class})
 public class OpenshiftApplicationTests {
 
 	@Test
---
+++ src/test/java/se/replior/tol5/openshift/TestConfiguration.java
@@ -0,0 +1,7 @@
+package se.replior.tol5.openshift;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfiguration {
+}
